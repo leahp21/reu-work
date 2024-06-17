3eff082f9a7e535a5b5a7a5e3c581ab7396e9c67
Leah Prince <leahprince@Jians-iMac.local>
2024-06-17 16:08:19 -0400

    improve loadable and nonloadable charts

--- top_10_anti_vax_ratio.py
+++ top_10_anti_vax_ratio.py
@@ -20,7 +20,7 @@ def bar_chart_mem_ratio(data_chart):
     plt.figure()
 
     plt.barh(y=data_chart['account name'],width=((data_chart['loadable archive_is mementos between 2019-2023']/ data_chart['total number of mementos archive.is']) * 100))
-    plt.barh(y=data_chart['account name'],width=((data_chart['wayback loadable mementos'] / data_chart['total number of wayback mementos between (2013 and mid 2019)']) * 100))
+    plt.barh(y=data_chart['account name'],width=((data_chart['wayback loadable mementos'] / data_chart['total number of wayback mementos between (2013 and 2018)']) * 100))
 
     plt.legend(["archive.is", "Wayback Machine"])
     plt.title('Ratio of Loadable Wayback Machine Mementos 2013-Mid 2019 and archive.is Mementos 2019-2023')
@@ -56,16 +56,34 @@ def bar_chart_wayback(data_chart):
 
     boop = np.arange(len(data_chart))
 
-    ax.barh(boop -0.2, data_chart['wayback loadable mementos'], 0.4)
-    ax.barh(boop + 0.2, data_chart['total number of wayback mementos between (2013 and mid 2019)'], 0.4)
+    #ax.barh(boop -0.2, , 0.4)
+    ax.barh(boop + 0.2, data_chart['total number of wayback mementos between (2013 and 2018)'], 0.4)
 
     ax.set(yticks=boop, yticklabels=data_chart['account name'])
 
     plt.legend(["Number of Loadable Mementos", "Total Number of Mementos"])
-    plt.title('Total Mementos and Scrapable Mementos from Wayback Machine for Top 10 Anti-Vaxxers Between 2013-Mid 2019')
+    plt.title('Total Mementos and Scrapable Mementos from Wayback Machine for Top 10 Anti-Vaxxers Between 2013-2018')
     plt.xlabel('Number of Wayback Machine Mementos')
     plt.ylabel('Instagram Account Name')
 
+    plt.show()
+
+def stacked_bar_chart_wayback(data_chart):
+    plt.barh(data_chart['account name'], width=data_chart['wayback loadable mementos'], color='g')
+    plt.barh(data_chart['account name'], width=(data_chart['total number of wayback mementos between (2013 and 2018)']) - (data_chart['wayback loadable mementos']), left = data_chart['wayback loadable mementos'], color='r')
+    plt.xlabel('Number of Replayable/unloadable Mementos')
+    plt.ylabel('Account Names')
+    plt.legend(["Loadable Mementos","Unloadable Mementos"])
+    plt.title('Replayable and Unloadable Wayback Machine Mementos for Top 10 Anti-vaxxers')
+    plt.show()
+
+def stacked_bar_chart_archive(data_chart):
+    plt.barh(data_chart['account name'], width=data_chart['loadable archive_is mementos between 2019-2023'], color='g')
+    plt.barh(data_chart['account name'], width=data_chart['total number of mementos archive.is'] - data_chart['loadable archive_is mementos between 2019-2023'], left=data_chart['loadable archive_is mementos between 2019-2023'], color='r')
+    plt.xlabel('Number of Replayable/Unloadable Mementos')
+    plt.ylabel('Account Names')
+    plt.legend(["Loadable Mementos","Unloadable Mementos"])
+    plt.title('Replayable and Unloadable Archive.today Mementos for Top 10 Anti-vaxxers')
     plt.show()
 
 if __name__ == "__main__":
@@ -79,3 +97,5 @@ if __name__ == "__main__":
     bar_chart_mem_ratio(account_data_df)
     bar_chart_archive_is(account_data_df)
     bar_chart_wayback(account_data_df)
+    stacked_bar_chart_wayback(account_data_df)
+    stacked_bar_chart_archive(account_data_df)
