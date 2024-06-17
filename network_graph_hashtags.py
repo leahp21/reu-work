4623940f3b137a76048862b08eb1e4c4e85d6ce2
Leah Prince <leahprince@Jians-iMac.local>
2024-06-17 16:07:33 -0400

    changed node color and size

--- network_graph_hashtags.py
+++ network_graph_hashtags.py
@@ -3,23 +3,42 @@ import re
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import math 
 
 def create_graph(hashtag_dict):
     G = nx.Graph()
+    color_map = {}
+    size_map = {}
+    font_map = {}
 
     for name, hashtag_list in hashtag_dict.items():
+
         print(name)
         if G.has_node(name) != True:
-            G.add_node(name, node_color='red', node_size=3000)
+            G.add_node(name)
+            color_map[name] = 'red'
+            size_map[name] = 900
+            font_map[name] = 12 
 
         for hashtag in hashtag_list:
             if G.has_node(hashtag):
                 G.add_edge(name, hashtag)
             else:
-                G.add_node(hashtag, node_color='blue', node_size=1500)
-                G.add_edge(name,hashtag)
+                if (search_for_other_hashtags(hashtag, hashtag_dict) >= 2):
+                    G.add_node(hashtag)
+                    color_map[hashtag] = 'yellow'
+                    size_map[hashtag] = 50
+                    font_map[hashtag] = 6
+                    G.add_edge(name,hashtag)
+
+    node_colors = [color_map[node] for node in G.nodes]
+    node_sizes = [size_map[node] for node in G.nodes]
+
+    pos = nx.spring_layout(G, k=.30)
+
+    plt.figure(3, figsize=(12,12))
+    nx.draw(G, with_labels=True, node_color=node_colors, font_size=5, node_size=node_sizes, pos = pos)
     
-    nx.draw(G, with_labels=True, font_size=7)
     plt.show()
 
 def generate_hashtag_dictionary(filename):
@@ -51,6 +70,17 @@ def generate_hashtag_dictionary(filename):
         hashtag_dictionary[account_name] = hashtag_list
     
     return hashtag_dictionary
+
+def search_for_other_hashtags(hashtag, dictionary):
+    hashtag_count = 0
+
+    for key, value in dictionary.items():
+
+        for each in value:
+            if each == hashtag:
+                hashtag_count += 1
+    
+    return hashtag_count
 
 if __name__ == "__main__":
     # get csv names, s
