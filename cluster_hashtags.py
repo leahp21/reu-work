37a24da4283452a4c3755e5c6f4eece4fb81f898
Leah Prince <leahprince@Jians-iMac.local>
2024-07-08 16:36:11 -0400

    updated hierarchical clustering

--- cluster_hashtags.py
+++ cluster_hashtags.py
@@ -1,16 +1,57 @@
 import pandas as pd
 from sklearn.cluster import KMeans
+from sklearn.cluster import AgglomerativeClustering
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import MinMaxScaler
+from scipy.cluster.hierarchy import dendrogram, linkage
 import argparse
 import re 
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-def map_non_numeric(dictionary):
+def prep_for_clustering(dictionary):
 
     new_df = pd.DataFrame()
 
+    new_dict = {'Username': dictionary.keys()}
+
+    new_df = pd.DataFrame.from_dict(new_dict)
+
+
+    for key, value_list in dictionary.items():
+
+        for value in value_list:
+            if value not in new_df.columns:
+                new_df[value] = hash_count_list_function(value, dictionary)
+
+
+    pd.set_option('display.max_rows', 500)
+    pd.set_option('display.max_columns', 10)
+    pd.set_option('display.width', 150)
+
+    print(new_df)
+
     '''
+    kmeans = KMeans(n_clusters=3)
+    kmeans.fit(new_df.drop('Username', axis=1))
+    clusters = kmeans.labels_
+    '''
+
+    Z = linkage(new_df.drop('Username', axis=1), method='ward')
+    new_df.set_index('Username', inplace=True)
+
+    plt.figure(figsize=(10, 8))
+    dendrogram(Z, labels=new_df.index, leaf_rotation=90)
+    plt.title('Dendrogram')
+    plt.xlabel('Words')
+    plt.ylabel('Distance')
+    plt.show()
+
+# Convert the scaled features back to a DataFrame
+
+def classify_hashtags(dictionary):
+
+    new_df = pd.DataFrame()
 
     food_hash = ["#seeds", "#food","#gmos","#avocado", "#kale", "#soup", "#soyfoods","#salad", "#wholefoods", "#superfood", "#superfoodsalad",
                  "#walnuts", "#beets", "#cucumber", "#asparagus", "#redpepper", "#apple", "#artichoke", "#spinach", "#realoliveoil", "#vinegarette",
@@ -18,86 +59,101 @@ def map_non_numeric(dictionary):
                  "#sauerkraut", "#coffee","#matchagreentea", "#avocados","#hassavocado","#healthysnack", "#healthyfood", "#almonds", "#cacao",
                  "#bonebroth", "#coconutoil", "#trailmix", "#driedcoconut", "#driedcranberries", "#pistachio", "#cashews", "#pumpkinseeds", "#mullberry", 
                  "#gojiberries", "#goldenraisins", "#greenjuice", "#vegetablejuice", "#carrots", "#cabbage", "#onions",
-                 "#brusselssprouts", "#leeks"]
+                 "#brusselssprouts", "#leeks", "#smoothie", "#raw", "#blueberries","#bananas", "#banana", "#juice", "#chocolate", "#fruit", "#coconut",
+                 "#mint", "#pie", "#wine", "#strawberries", "#sweetpotato", "#turmeric", "#curcumin", "#goldenmilk", "#gmo", "#pudding",
+                 "#carrot", "#dessert", "#veggies", "#honey", "#veggie", "#rawfood", "#lemon", "#falafel", "#garlic", "#cumin", "#salt", "#sesameseeds", "#soy", "#nogmo",
+                 "#sugar", "#watermelon", "#celery", "#broccoli", "#cantaloupe", "#radish", "#tomato", "#hymalayansalt", "#blackcuminseed", "#maca",
+                 "#citrus", "#capers", "#seafood", "#mushrooms", "#beans", "#nuts", "#wildsalmon", "#keto", "#ketodiet", "#castoroil", "#rice", "#cassava", "#maize"] 
 
     disease_hash = ["#aids", "#hpv","#transversemyelitis","#hepatitisb","#multiplesclerosis", "#autism", "#inflammation", "#sids",
                     "#sidsawareness", "#suddendeath","#suddeninfantdeathsyndrome","#cancer", "#diabetes", "#flu",
-                    "#infertility", "#breastcancer", "#hypothyroidism", "#dementia"]
+                    "#infertility", "#breastcancer", "#hypothyroidism", "#dementia", "#headaches", "#depression", "#chronicfatigue", "#seizure",
+                    "#decreasedvision", "#bloodpressure", "#autoimmunedisorder", "#hyperthyroidism", "#brainfog", "#hiv", "#defeatmalaria", "#malaria", "#worldaidsday",
+                    "#rethinkhiv", "#hivstatus", "#endpolio", "#trypanosomiasis", "#africansleepingsickness", "#infectiousdisease"]
 
-    health_hash = ["#vitaminc", "#vitamind","#iodine", "#health", "#antibodyenhancement","#vegan", "#veganfitness", "#vegetarian", "#plantprotein", "#plantbased",
+    health_hash = ["#vitaminc", "#vitamind","#iodine", "#health", "#antibodyenhancement","#vegan", "#vegetarian", "#veganfitness", "#vegetarian", "#plantprotein", "#plantbased",
                    "#centralnervoussystem","#naturalhealth","#brainhealth", "#gut", "#calcium", "#magnesium", "#copper", "#iron", "#zinc", "#protein",
                    "#thyroid", "#kidneys", "#antioxidants","#vitamins","#minerals","#enzymes", "#fiber", "#cholesterol", "#triglycerides", "#healthyfats", 
-                   "#relievepain", "#stomach", "#bacteria", "#fungi", "#viruses", "#pathogens", "#illness", "#microbes", "#probiotic", "#anticancer", "#cancerfighter"]
+                   "#relievepain", "#stomach", "#bacteria", "#fungi", "#viruses", "#pathogens", "#illness", "#microbes", "#probiotic", "#anticancer", "#cancerfighter",
+                   "#gluten", "#healthy", "#detox", "#holistic", "#chiropractors", "#shoulder", "#injury", "#stemcells", "#bones", "#plant", "#alkaline", "#natural",
+                   "#organic", "#diet", "#mri", "#healthylifestyle", "#instahealth", "#gmofree", "#naturalhealing", "#organiclife", "#quitsmoking", "#cancerprevention",
+                   "#hearthealth", "#bonehealth", "#healthychoices", "#cancerfighting", "#holistichealing", "#hyperbaricoxygentherapy", "#oxygenflow", 
+                   "#tips4health", "#coffeeenema", "#globalhealth"]
 
     covid_hash = ["#coronavirus", "#covid_19", "#covid", "#covidvaccine","#pandemic", "#lockdown", "#twindemic", "#covid", "#covid19", "#wuhan", "#labcreated",
-                  "#chinavirus", "#drlimengyan", "#nomasks", "#nomandates", "#coronafalsealarm","#vaxvsunvax","#pland3mic"]
+                  "#chinavirus", "#drlimengyan", "#nomasks", "#nomandates", "#coronafalsealarm","#vaxvsunvax","#pland3mic", "#mask", "#contagionmyth", "#maskyourkids",
+                  "#noforrealmaskyourkids", ]
     
     vaccine_hash = ["#vaccine", "#vaccinesafety", "#vaccineinjury", "#vaers", "#hpvvaccine", "#flushot", "#vaccines", "#masteringvaccineinfo",
                     "#vaccinetrials", "#noforcedflushots", "#vaccineeducation", "#vaccineroulette", "#4monthshots","#vaccineriskawareness", "#vaccinesarepoison",
-                    "#vaccinefree","#deathaftervaccines","#vaccinations","#noshots","#vaccineskill","#informedconsent", "#learntherisk"]
+                    "#vaccinefree","#deathaftervaccines","#vaccinations","#noshots","#vaccineskill","#informedconsent", "#learntherisk", "#informedconsent", "#childrenshealthdefense",
+                    "#dtap", "#nvic", "#mmr", "#truthaboutvaccines", "#truthaboutcancer", "#thetruthaboutcancer", "#ttac", "#medicalfreedomactivist",
+                    "#medicalfreedomofchoice", "#medicalfreedomwarrior", "#medicalfreedom", "#wapf", "#vaccineswork"]
+    
+    other_conspiracy = ["#chemtrails", "#geoengineering", "#truther", "#alexjones", "#cloudseeding", "#rockefeller", "#rothschild", "#badscience"]
 
-    pharmaceuticals_hash = ["#bigpharma", "#bigchemical", "#billgates", "#gates","#gatesfoundation", "#informedconsent",
-                            "#pfizer", "#moderna", "#merck", "#gardasil", "#astrazeneca", "#pharma"]
+    pharmaceuticals_hash = ["#bigpharma", "#bigchemical", "#billgates", "#gates","#gatesfoundation",
+                            "#pfizer", "#moderna", "#merck", "#gardasil", "#astrazeneca", "#pharma", "#iheartpharma"]
 
-    gov_hash = ["#anthonyfauci", "#fauci", "#corruption", "#bigbrother", "#censorship", "#censorshipisreal", "#cdc", "#nih", "#fda","#cia", "#surveillance", "#who"]
-
-    ''' 
+    gov_hash = ["#anthonyfauci", "#fauci", "#corruption", "#bigbrother", "#censorship", "#censorshipisreal", "#cdc", "#nih", "#fda","#cia", "#surveillance", "#who",
+                "#politics", "#tyranny", "#government", "#1984", "#orwell", "#aldoushuxley", "#huxley", "#bravenewworld", "#georgeorwell", "#agenda2030", ]
+    
 
     for key, value_list in dictionary.items():
 
+        vaccine_count = 0
+        gov_count = 0
+        pharma_count = 0
+        covid_count = 0
+        health_count = 0
+        disease_count = 0
+        food_count = 0
+        other_conspiracy_count = 0
+        extra_count = 0
+
         for value in value_list:
-            new_df = new_df.append({"Username": key, "Hashtag": value}, ignore_index=True)
+            if value in food_hash:
+                food_count += 1
+            elif value in vaccine_hash:
+                vaccine_count += 1
+            elif value in gov_hash:
+                gov_count += 1
+            elif value in pharmaceuticals_hash:
+                pharma_count += 1
+            elif value in covid_hash:
+                covid_count += 1
+            elif value in health_hash:
+                health_count += 1
+            elif value in disease_hash:
+                disease_count += 1
+            elif value in other_conspiracy:
+                other_conspiracy_count += 1
+            else:
+                extra_count += 1
+        
+        total = vaccine_count + gov_count + pharma_count + covid_count +health_count + disease_count + food_count + other_conspiracy_count + extra_count
 
-    pd.set_option('display.max_rows', 500)
-    pd.set_option('display.max_columns', 2)
-    pd.set_option('display.width', 150)
-
-    print(new_df)
-
-
-    ''' machine learning stuff '''
-    X = new_df
-    y = new_df['Username']
-
-    le = LabelEncoder()
-
-    cols = X.columns
-
-    X['Username'] = le.fit_transform(X['Username'])
-    X['Hashtag'] = le.fit_transform(X['Hashtag'])
-
-    ms = MinMaxScaler()
-
-    X = ms.fit_transform(X)
-
-    X = pd.DataFrame(X, columns=[cols])
-
-    print(X, y)
-
-
-# Convert the scaled features back to a DataFrame
+        new_df = new_df.append({'Username': key, 'Vaccine Count': vaccine_count, 'Pharmaceutical Count': pharma_count, 'COVID Count': covid_count, 'Health Count': health_count, 'Health Condition Count': disease_count,
+                       'Food Count': food_count, 'Conspiracy Count': other_conspiracy_count, 'Extraneous Count': extra_count, 'Total Count':total}, ignore_index=True)
+        
+        new_df.set_index('Username')
+        
+    print (new_df)
 
 
-    cs = []
-    for i in range(1, 11):
-        kmeans = KMeans(n_clusters = i, init = 'k-means++', max_iter = 300, n_init = 10, random_state = 0)
-        kmeans.fit(X)
-        cs.append(kmeans.inertia_)
+def hash_count_list_function(target_hashtag, dictionary):
+    hash_count_list = []
 
+    for key, value_list in dictionary.items():
+        hash_count = 0
+
+        for hash in value_list:
+            if hash == target_hashtag:
+                hash_count += 1
+        
+        hash_count_list.append(hash_count)
     
-    plt.plot(range(1, 11), cs)
-    plt.title('The Elbow Method')
-    plt.xlabel('Number of clusters')
-    plt.ylabel('CS')
-    plt.show()
-
-    
-
-    kmeans = KMeans(n_clusters=3,random_state=0)
-    kmeans.fit(X)
-
-    plt.scatter(X[X.columns[1]], X[X.columns[0]], c=kmeans.labels_)
-    plt.show()
+    return hash_count_list
 
 
 def generate_hashtag_dictionary(filename):
@@ -117,14 +173,14 @@ def generate_hashtag_dictionary(filename):
         hashtag_list = []
 
         df = pd.read_csv(csv)
+        df.drop_duplicates
         
         for index, row in df.iterrows():
             if pd.notna(row['Hashtags']):
                 temp_list = row['Hashtags'].split(',')
 
                 for hash in temp_list:
-                    if hash.lower() not in hashtag_list:
-                        hashtag_list.append(hash.lower())
+                    hashtag_list.append(hash.lower())
         
         hashtag_dictionary[account_name] = hashtag_list
     
@@ -138,4 +194,5 @@ if __name__ == "__main__":
 
     dictionary = generate_hashtag_dictionary(args.csv_text_file)
 
-    map_non_numeric(dictionary)
+    prep_for_clustering(dictionary)
+    # classify_hashtags(dictionary)
